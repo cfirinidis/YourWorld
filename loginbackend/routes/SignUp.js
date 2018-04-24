0121d5ddfa799bccf7cec65d1c20c3c2fb5376e0
Your Name <chamburger1@gmail.com>
2018-04-24 14:20:53 -0400

    files from chris branch into mine

--- loginbackend/routes/SignUp.js
+++ loginbackend/routes/SignUp.js
@@ -18,6 +18,7 @@ router.post('/', function(req, res, next) {
 
 
 	connection.query(
+
 		"INSERT INTO user ( username, password, email) VALUES (?, ?, ?)",
 		[username, password, email], function(err, row, field){
 			if(err){
@@ -26,11 +27,49 @@ router.post('/', function(req, res, next) {
 				res.send({'success': false, 'message': 'Could not connect to DB'});
 			}
 			else{
-		
 			res.send({ 'success': true});
 			}
 
 		});
+
+	connection.query("SELECT username FROM user GROUP BY username HAVING count(*) > 1",
+	[username, password, email], function(err, row, field){
+		if(err){
+			console.log(err);
+			res.send({'success': false, 'message': 'Could not connect to DB'});
+		}/*
+		if(row != null){
+			console.log('Username already already exists.');
+			
+		}else{
+			
+			connection.query(
+
+				"INSERT INTO user ( username, password, email) VALUES (?, ?, ?)",
+				[username, password, email], function(err, row, field){
+					if(err){
+						
+						console.log(err);
+						res.send({'success': false, 'message': 'Could not connect to DB'});
+					}
+					else{
+					res.send({ 'success': true});
+					}
+		
+				});
+		
+		}
+		*/
+
+		console.log('username:' + username);
+		console.log('password:' + password);
+		console.log('email: ' + email);
+		console.log('Heres the error(probably null):' + err);
+		console.log('Heres the row:' + row);
+		console.log('Heres the field: ' + field);
+	});
+
+
 });
 
 module.exports = router;
