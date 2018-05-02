36151ffecee155ddeafc1e4c16022d0be93305a2
cfirinidis <cfirinidis@gmail.com>
2018-05-02 12:56:12 -0400

    map update

--- urwrld/app/components/Login.js
+++ urwrld/app/components/Login.js
@@ -75,7 +75,7 @@ export default class Login extends React.Component {
 			
 	login = () => {
 
-		fetch('http://146.95.79.164:3000/users', {// sync IP address to expo application
+		fetch('http://146.95.79.173:3000/users', {// sync IP address to expo application
 			method: 'POST',
 			headers: {
 				'Accept': 'application/json',
