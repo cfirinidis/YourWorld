fbe89a9e907d15865c3f6b152987e9c7eee35596
christopherf242 <cferguson575@yahoo.com>
2018-05-13 11:22:26 -0400

    Home.js update #1

--- urwrld/app/components/Home.js
+++ urwrld/app/components/Home.js
@@ -18,13 +18,158 @@ import Places from './Places';
 import Maps from './Maps';
 import Loading from './Loading';
 
+//The rounTo function takes in a number n as the first parameter and the number of digits as the second parameter.
+//Online Source: https://stackoverflow.com/questions/15762768/javascript-math-round-to-two-decimal-places?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+function roundTo(n, digits) {
+    var negative = false;
+    if (digits === undefined) {
+        digits = 0;
+    }
+        if( n < 0) {
+        negative = true;
+      n = n * -1;
+    }
+    var multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    n = (Math.round(n) / multiplicator).toFixed(2);
+    if( negative ) {    
+        n = (n * -1).toFixed(2);
+    }
+    return n;
+}
+
+//Online Source:
+//https://stackoverflow.com/questions/43176862/get-current-location-latitude-and-longitude-in-reactnative-using-react-native-m
+
 //Welcome Page
 export default class Home extends React.Component {
+	state = {
+		mapRegion: null,
+		lastLat: null,
+		lastLong: null,
+	  }
     //Not sure if needed
     static navigationOptions = {
-            title: 'Profile',
-        };
-        // The button labeled Checkin goes to Loading.js(should we put something extra in Loading.js)
+            title: 'Loading',
+		};
+		
+		// The button labeled Checkin goes to Loading.js(should we put something extra in Loading.js)
+		componentDidMount() {
+			this.watchID = navigator.geolocation.watchPosition((position) => {
+			  // Create the object to update this.state.mapRegion through the onRegionChange function
+			  let region = {
+				latitude:       position.coords.latitude,
+				longitude:      position.coords.longitude,
+				latitudeDelta:  0.00922*1.5,
+				longitudeDelta: 0.00421*1.5
+			  }
+			  var userLatitude = 0;
+			  var userLongitude = 0;
+			  this.onRegionChange(region, region.latitude, region.longitude);
+			});
+		  }
+		
+		  onRegionChange(region, lastLat, lastLong) {
+			this.setState({
+			  mapRegion: region,
+			  // If there are no new values set use the the current ones
+			  lastLat: lastLat || this.state.lastLat,
+			  lastLong: lastLong || this.state.lastLong
+			});
+			
+		   
+			userLatitude = lastLat
+			userLongitude = lastLong;
+		
+			 userLatitude = roundTo(userLatitude, 3);
+			 userLongitude = roundTo(userLongitude, 3);
+			 console.log('userLatitude rounded to 3 decimal places: ' + userLatitude);
+			 console.log('userLongitude rounded to 3 decimal places: ' + userLongitude);
+		
+			
+		  }
+		
+		
+		  componentWillUnmount() {
+			navigator.geolocation.clearWatch(this.watchID);
+		  }
+		
+		  onMapPress(e) {
+			console.log(e.nativeEvent.coordinate.longitude);
+			let region = {
+			  latitude:       e.nativeEvent.coordinate.latitude,
+			  longitude:      e.nativeEvent.coordinate.longitude,
+			  latitudeDelta:  0.00922*1.5,
+			  longitudeDelta: 0.00421*1.5
+			}
+			this.onRegionChange(region, region.latitude, region.longitude);
+		  }
+
+
+		  getloc = () => {
+			console.log("called getloc")
+	   
+		   fetch('http://192.168.13.14:3000/users', {// sync IP address to expo application
+			 method: 'POST',
+			 headers: {
+			   'Accept': 'application/json',
+			   'Content-Type': 'application/json',
+			 },
+			 body: JSON.stringify({
+			   lat: this.state.lastLat,
+			   lng: this.state.lastLong
+			 })
+		   })
+	   
+		   .then((response)=> response.json())
+		   .then ((res) => {
+	   
+			 if(res.success === true){
+			   //AsyncStorage.setItem('user', res.user);
+			   this.props.navigation.navigate('Loading');
+			 }
+			 else{
+			   alert(res.message);
+			 }
+		   })
+		   .done();
+		 }//end of getloc
+	   
+	   
+
+		//   pressCheckin = () => {
+		// 	const { latitude } = this.state ;
+		// 	const { password } = this.state ;
+		// 	const { email } = this.state ;
+	
+
+		
+	
+		// 	fetch('http://192.168.13.14:3000/SignUp', { // sync IP address to expo application
+		// 		method: 'POST',
+		// 		headers: {
+		// 			'Accept': 'application/json',
+		// 			'Content-Type': 'application/json',
+		// 		},
+		// 		body: JSON.stringify({
+		// 			username: this.state.username,
+		// 			password: this.state.password,
+		// 			email: this.state.email,
+		// 		})
+		// 	})
+	
+		// 	.then ((response) => response.json())
+		// 	.then ((res) => {
+		// 		if(res.success === true){
+		// 			this.props.navigation.navigate('Home');
+		// 		}
+		// 		else{
+		// 			alert(res.message);
+		// 		}
+		// 	})
+		// 	.done();
+		// }
+		
 	render() {
 		return(
 		<KeyboardAvoidingView behavior='padding' style={styles.wrapper}>
@@ -42,8 +187,8 @@ export default class Home extends React.Component {
 
 				<TouchableOpacity
 					style={styles.btn}
-					onPress={this.Loading}>
-					<Text> Checkin {/* This button goes to profile{this.Profile navigates to Profile.js}	*/}</Text>
+					onPress={this.getloc}>
+					<Text> Checkin(goes to Loading Page) {/* This button goes to profile{this.Profile navigates to Profile.js}	*/}</Text>
 				</TouchableOpacity>
 
 			</View>
@@ -56,8 +201,8 @@ export default class Home extends React.Component {
 Profile = () => {
 		this.props.navigation.navigate('Profile');
 	}
-    Loading = () => {
-        this.props.navigation.navigate('Loading');
+Loading = () => {
+        this.props.navigation.navigate('Maps');
     }
 }
 
--- urwrld/app/components/Maps.js
+++ urwrld/app/components/Maps.js
@@ -1,225 +1,159 @@
-import React from 'react';
-import{
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    KeyboardAvoidingView,
-  Dimensions,
+import React, { Component } from 'react';
+import {
   AppRegistry,
+  StyleSheet,
+  Text,
+  View
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import MapView, {Callout, Circle, Marker, Polygon} from 'react-native-maps'
+import MapView from 'react-native-maps'; // 0.21.0
 
-import Login from './Login';
-import SignUp from './SignUp';
-import Profile from './Profile';
-
-let { width, height } = Dimensions.get('window');
-
-const ASPECT_RATIO = width / height;
-const LATITUDE = 0;
-const LONGITUDE = 0;
-const LATITUDE_DELTA = 0.0008;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-
-
-export default class Maps extends React.Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
-      markers: [ {latitude: 40.629718, longitude: -73.900231},/* {latitude: region.latitude, longitude: region.longitude} */], 
-      inPolygon: false,
-    };
-    /*
-    var hunterCollege = {latitude: 40.7685, longitude: -73.9646};
-    markers.push(hunterCollege);
-    */
+//source: Stack Exchange
+function roundTo(n, digits) {
+  var negative = false;
+  if (digits === undefined) {
+      digits = 0;
+  }
+      if( n < 0) {
+      negative = true;
+    n = n * -1;
+  }
+  var multiplicator = Math.pow(10, digits);
+  n = parseFloat((n * multiplicator).toFixed(11));
+  n = (Math.round(n) / multiplicator).toFixed(3);
+  if( negative ) {    
+      n = (n * -1).toFixed(3);
+  }
+  return n;
+}
+//These variables are defined outside of the class, but are changed inside the changeRegion function.
+var userLatitude = 0;
+var userLongitude = 0;
+export default class Maps extends Component {
+  state = {
+    mapRegion: null,
+    lastLat: null,
+    lastLong: null,
   }
 
-  
-
-
   componentDidMount() {
-    
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({
-          region: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-          },
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      // Create the object to update this.state.mapRegion through the onRegionChange function
+      let region = {
+        latitude:       position.coords.latitude,
+        longitude:      position.coords.longitude,
+        latitudeDelta:  0.00922*1.5,
+        longitudeDelta: 0.00421*1.5
+      }
+      // var userLatitude = 0;
+      // var userLongitude = 0;
+      this.onRegionChange(region, region.latitude, region.longitude);
+    });
+  }
 
-        });
-        let pointXCoordinateDidMount = position.coords.latitude;
-        let pointYCoordinateDidMount = position.coords.longitude;
-
-        console.log('In ComponentDidMount: Heres the user location x coordinate: ' + pointXCoordinate);
-        console.log('In ComponeneDidMount: Heres the user location y coordinate: ' + pointYCoordinate);
-      },
-    (error) => console.log(error.message),
-    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
+  onRegionChange(region, lastLat, lastLong) {
+    this.setState({
+      mapRegion: region,
+      // If there are no new values set use the the current ones
+      lastLat: lastLat || this.state.lastLat,
+      lastLong: lastLong || this.state.lastLong
+    });
     
-    //watch position is a callback function that updates everytime the app 
-    // detects a new user location
-    // this.watchID = navigator.geolocation.watchPosition(
-    //   position => {
-    //     this.setState({
-    //       region: {
-    //         latitude: position.coords.latitude,
-    //         longitude: position.coords.longitude,
-    //         latitudeDelta: LATITUDE_DELTA,
-    //         longitudeDelta: LONGITUDE_DELTA,
-    //       },
-          
-    //     });
-    //   }
-    // );
+   
+    userLatitude = lastLat
+    userLongitude = lastLong;
+
+     userLatitude = roundTo(userLatitude, 3);
+     userLongitude = roundTo(userLongitude, 3);
+     console.log('userLatitude rounded to 3 decimal places: ' + userLatitude);
+     console.log('userLongitude rounded to 3 decimal places: ' + userLongitude);
 
     
+  }
 
-    if(this._isInPolygon(this.state.region.latitude, this.state.region.longitude)){
-      console.log('user is at hunter college');
-    }else{
-      console.log('user is not at hunter college');
-    }
-  }//component did mount
 
-  //Before we render, we just clear out the watchID variable
   componentWillUnmount() {
     navigator.geolocation.clearWatch(this.watchID);
   }
 
-  
-  _isInPolygon = (pointXCoordinate, pointYCoordinate) => {
- 
-
-    console.log('In _isInPolygon function: Heres the user location x coordinate: ' + pointXCoordinate);
-    console.log('In _isInPolygon functoin: Heres the user location y coordinate: ' + pointYCoordinate);
-
-    //Euclidean algorithm(from pythagorean's theorem)
-    var xAxisDistance = Math.pow((40.7685 - pointXCoordinate), 2);
-    console.log('Heres x axis distance: ' + xAxisDistance);
-    var yAxisDistance = Math.pow((-73.9646 - pointYCoordinate), 2);
-    console.log('Heres y axis distance: ' + yAxisDistance);
-    var sumOfDistance = xAxisDistance + yAxisDistance;
-    console.log('Heres the sum of distance formula: ' + sumOfDistance + '\n');
-   
-
-    //Next we calculate the size of hunter college(center - edge): center(40.768587, -73.964685)
-    //  Side of Hunter College: 40.768778, -73.965217
-
-
-    var xCoordinateHunterCollege = Math.pow((40.768587 - 40.768778), 2);
-    console.log('Heres x coordinate of Hunter College: ' + xCoordinateHunterCollege);
-    var yCoordinateHunterCollege = Math.pow((-73.964685 - -73.965217), 2);
-    console.log('Heres y coordinate of Hunter College: ' + yCoordinateHunterCollege);
-    var sumOfDistanceHunterCollege = xCoordinateHunterCollege + yCoordinateHunterCollege;
-    
-    console.log('Heres the sum of distance Hunter College formula: ' + sumOfDistanceHunterCollege + '\n');
-    if(sumOfDistance < sumOfDistance){
-      return true;
-    }else{
-      return false;
+  onMapPress(e) {
+    console.log(e.nativeEvent.coordinate.longitude);
+    let region = {
+      latitude:       e.nativeEvent.coordinate.latitude,
+      longitude:      e.nativeEvent.coordinate.longitude,
+      latitudeDelta:  0.00922*1.5,
+      longitudeDelta: 0.00421*1.5
     }
+    this.onRegionChange(region, region.latitude, region.longitude);
   }
 
   render() {
     return (
-      //MapView renders a map and you're the center
-      <MapView
-      //  provider={ PROVIDER_GOOGLE }
-        style={ styles.container }
-       // customMapStyle={ RetroMapStyles }
-        showsUserLocation={ true }
-        region={ this.state.region }
-        onRegionChange={ region => this.setState({region}) }
-        onRegionChangeComplete={ region => this.setState({region}) }
-      >
-      
-        {/* <MapView.Marker
-          coordinate={this.state.region}
-           title={"Hunter College"} description={"School"}
-        /> */}
-        <MapView.Marker
-          coordinate={{latitude: 40.7685, longitude: -73.9646}}
-           title={"Hunter College"} description={"School"}
-        />
-        <View style={styles.radius}>
-          <View style={styles.marker}/>
-        
-        </View>
-        <MapView.Polygon coordinate= {this.state.markers} />
-
-      </MapView>
+      <View style={{flex: 1}}>
+        <Text style={styles.header}>Yeoo let's go {/*Put location name   */}</Text> 
+        {/* <MapView
+          style={styles.map}
+          region={this.state.mapRegion}
+          showsUserLocation={true}
+          followUserLocation={true}
+          onRegionChange={this.onRegionChange.bind(this)}
+          onPress={this.onMapPress.bind(this)}>
+          <MapView.Marker
+            coordinate={{
+              latitude: (this.state.lastLat + 0.00050) || -36.82339,
+              longitude: (this.state.lastLong + 0.00050) || -73.03569,
+            }}>
+            <View>
+              <Text style={{color: '#000'}}>
+                { this.state.lastLong } / { this.state.lastLat }
+              </Text>
+            </View>
+          </MapView.Marker>
+        </MapView> */}
+      </View>
     );
   }
 }
-var LatLng = [{
-  latitude: 39.749632,
-  longitude: -105.000363,
-}];
-/*
-var markers = [
-  {
-    latitude: 40.7685, 
-    longitude: -73.9646 
-   //title: 'Foo Place',
-    //subtitle: '1234 Foo Drive'
-    
-  }
-];
-*/
 
 const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
   wrapper: {
 		flex: 1,
-  },
-  radius: {
-    height: 50,
-    width: 50,
-    borderRadius: 50, 
-    borderRadius: 50 / 2,
-    overflow: 'hidden',
-    backgroundColor: 'rgba(0, 122, 255, 0.1)',
-    borderWidth: 1,
-    borderColor: 'rgba(0, 112, 255, 0.3)',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  marker: {
-    height: 20,
-    width: 20, 
-    borderWidth: 3,
-    borderColor: 'white',
-    borderRadius: 20 / 2,
-    overflow: 'hidden',
-    backgroundColor: '#007AFF'
-  },
-  container: {
-    flex: 1,
-    height: '100%',
-    width: '100%',
-  },
-  map: {
-    position: 'absolute',
-    top: 10,
-    left: 0,
-    right: 0,
-  //  botttom: 0
-  } 
-});
+	},
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#dc6900',
+		paddingLeft: 40,
+		paddingRight: 40,
+	},
+	header: {
+		fontSize: 24,
+		marginBottom: 60,
+		color: '#fff',
+		fontWeight: 'bold',
+	},
+	textInput: {
+		alignSelf: 'stretch',
+		padding: 16,
+		marginBottom: 20,
+		backgroundColor: '#fff',
+	},
+	btn: {
+		alignSelf: 'stretch',
+		backgroundColor: '#d4d8d4',
+		padding:20,
+		alignItems: 'center',
+	},
+	SignUp: {
+		//fontSize: 18,
+		marginTop: 60,
+		//color: '#fff',
+		//fontWeight: 'bold',
+		},
 
+});
 
 AppRegistry.registerComponent('Maps', () => Maps);
